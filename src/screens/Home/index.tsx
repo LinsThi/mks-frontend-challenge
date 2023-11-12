03c6eb01263746427f9fe0b9bea901a717d1c89c
ThiagoLinsDev <135272380+ThiagoLinsDev@users.noreply.github.com>
2023-11-12 16:27:00 -0300

    feat: implementing Home Screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,53 @@
-import { Container } from "./styles";
+import {
+  ButtonBuy,
+  ButtonTitle,
+  Container,
+  IconButton,
+  ImageProduct,
+  InfoInSameLine,
+  InformationProduct,
+  ListProducts,
+  NameProduct,
+  Product,
+  ProductInfo,
+  TextValueProduct,
+  ValueProduct,
+} from "./styles";
+
+import shoppingIcon from "../../assets/shopping.svg";
+import AsideCart from "../../components/AsideCart";
 
 function Home() {
   return (
     <Container>
-      <h2>Hello</h2>
+      <ListProducts>
+        <Product>
+          <ImageProduct
+            src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/41-nc-alum-red-sport-band-red-s9?wid=1200&hei=630&fmt=jpeg&qlt=95&.v=1693325468410"
+            alt="image-product"
+          />
+
+          <ProductInfo>
+            <InfoInSameLine>
+              <NameProduct>Apple WatchApple Wa</NameProduct>
+              <ValueProduct>
+                <TextValueProduct>R$399</TextValueProduct>
+              </ValueProduct>
+            </InfoInSameLine>
+
+            <InformationProduct>
+              Redesigned from scratch and completely revised.
+            </InformationProduct>
+          </ProductInfo>
+
+          <ButtonBuy>
+            <IconButton src={shoppingIcon} alt="button-icon" />
+            <ButtonTitle>COMPRAR</ButtonTitle>
+          </ButtonBuy>
+        </Product>
+      </ListProducts>
+
+      <AsideCart />
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,7 +1,97 @@
 import styled from "styled-components";
+import IconSVG from "../../components/IconSVG";
 
-export const Container = styled.div`
+export const Container = styled.section`
   display: flex;
   flex-direction: column;
   height: 83vh;
+`;
+
+export const ListProducts = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(270px, 1fr));
+  gap: 25px;
+  margin: 50px;
+`;
+
+export const Product = styled.div`
+  border-radius: 8px;
+  box-shadow: 0px 2px 8px 0px #00000022;
+`;
+
+export const ImageProduct = styled.img`
+  width: 100%;
+
+  mix-blend-mode: multiply;
+`;
+
+export const ProductInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 10px;
+`;
+
+export const InfoInSameLine = styled.div`
+  display: flex;
+`;
+
+export const NameProduct = styled.span`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 19px;
+  letter-spacing: 0px;
+  display: flex;
+  flex: 1;
+`;
+
+export const InformationProduct = styled.span`
+  font-size: 10px;
+  font-weight: 300;
+  line-height: 12px;
+
+  margin: 15px 0 10px;
+`;
+
+export const ValueProduct = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 60px;
+  height: 40px;
+
+  background-color: ${({ theme }) => theme.COLORS.DARK_GRAY};
+  border-radius: 8px;
+`;
+
+export const TextValueProduct = styled.span`
+  font-size: 15px;
+  font-weight: 700;
+  line-height: 15px;
+  letter-spacing: 0px;
+
+  color: ${({ theme }) => theme.COLORS.MAIN};
+`;
+
+export const ButtonBuy = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+
+  background-color: ${({ theme }) => theme.COLORS.PRIMARY};
+  width: 100%;
+  padding: 5px 20px;
+  border-radius: 0 0 8px 8px;
+`;
+
+export const IconButton = styled(IconSVG)``;
+
+export const ButtonTitle = styled.span`
+  color: ${({ theme }) => theme.COLORS.MAIN};
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 18px;
+  letter-spacing: 0px;
+  text-align: left;
 `;

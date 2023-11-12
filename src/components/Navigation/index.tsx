2f62e8f1af538aadbe151841070e5fb43e747756
ThiagoLinsDev <135272380+ThiagoLinsDev@users.noreply.github.com>
2023-11-12 16:27:25 -0300

    feat: implementing useAsideCart

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -9,8 +9,13 @@ import {
 } from "./styles";
 
 import cartIcon from "../../assets/cart.svg";
+import { useAsideCard } from "../AsideCart/store";
 
 function Navigation() {
+  const {
+    actions: { handleOpenCart },
+  } = useAsideCard();
+
   return (
     <Container>
       <Logo>
@@ -18,7 +23,7 @@ function Navigation() {
         <SubNameLogo>Sistemas</SubNameLogo>
       </Logo>
 
-      <Button>
+      <Button onClick={handleOpenCart}>
         <Icon src={cartIcon} alt="cart-icon" size={18} />
         <QuantityItensInCart>0</QuantityItensInCart>
       </Button>

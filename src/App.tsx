14fd189443d6c77df4f447ba0dce877974842775
ThiagoLinsDev <135272380+ThiagoLinsDev@users.noreply.github.com>
2023-11-12 14:40:22 -0300

    feat: implementing Navigation and Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/global";
 import theme from "./theme";
 import Home from "./screens/Home";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <ThemeProvider theme={theme.light}>
+      <Navigation />
+
       <Home />
+
+      <Footer />
 
       <GlobalStyle />
     </ThemeProvider>

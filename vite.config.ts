c70432e45bd9252c57d55f039f5e0e7e9a43a07a
ThiagoLinsDev <135272380+ThiagoLinsDev@users.noreply.github.com>
2023-11-13 15:15:44 -0300

    build: adding jest and testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@components": resolve(__dirname, "src/shared/components"),
+      "@src": resolve(__dirname, "src"),
+      "@pages": resolve(__dirname, "src/pages"),
+    },
+  },
   plugins: [react()],
-})
+});
